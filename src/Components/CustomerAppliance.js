1d8a29815c60299e013ade0bc0b3eeea348fd67f
Pratikshit Agrahari <70504376+Pratikshit09@users.noreply.github.com>
2021-04-23 12:40:27 +0530

    Added Customer AC,Appliance,Barber,Carpenter,Electrician,Plumber
    Added Customer AC,Appliance,Barber,Carpenter,Electrician,Plumber

---
+++ src/Components/CustomerAppliance.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import {Card} from 'reactstrap';
+import {Link} from 'react-router-dom';
+class CustomerAppliance extends Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+        }
+    }
+
+    render(){
+        return(
+            <>
+            <div className="row">
+                <div className="col-12 container-fluid">
+                    <h2 className="feature-heading container-fluid ">Appliance Repair</h2>
+                    <hr className="feature-line" />
+                </div>
+            </div>
+            <div className="container-fluid row mb-5">
+            {this.props.ApplianceRepair.map((Appliance)=>{
+                return(
+                        <div className="col-md-6 mt-3">
+                            <div className="card">
+                                <div className="card-header color" style={{fontWeight:'bold'},{fontSize:"20px"}}>
+                                    {Appliance.ServiceProvider_Username}
+                                </div>
+                                <div className="card-body">
+                                    <div className="row">
+                                        <h5 className="card-title col-md-5">Name: {Appliance.ServiceProvider_Username}
+                                        </h5>
+                                        <h5 className="card-title col-md-7">MobileNo: {Appliance.MobileNo}
+                                        </h5>
+                                    </div>
+                                    <div className="row">
+                                        <h5 className="card-title col-md-5">Average Price: {Appliance.Price}
+                                        </h5>
+                                        <h5 className="card-title col-md-7">Gender: {Appliance.Gender}
+                                        </h5>
+                                    </div>
+                                    <div className="row">
+                                        <h5 className="card-title col-md-5">City: {Appliance.City}
+                                        </h5>
+                                        <h5 className="card-title col-md-7">Occupation: {Appliance.Occupation}
+                                        </h5>
+                                    </div>
+                                    <div className="row">
+                                        <h5 className="card-title col-md-12">EmailID: {Appliance.email}
+                                        </h5>
+                                    </div>
+                                    <div className="row">
+                                        <h5 className="card-title col-md-12">Description: {Appliance.Description}
+                                        </h5>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <Link to={`/Customer/Booking/${Appliance.ServiceProvider_Username}`}>
+                                                <button className="btn btn-primary btn-md btn-block mt-2 color"  ><span className="fa fa-sign-in-alt fa-lg"></span>Book Service</button>
+                                            </Link>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <Link to={`/Customer/Feedback/${Appliance.ServiceProvider_Username}`}>
+                                                <button className="btn btn-primary btn-md btn-block mt-2 color"  ><span className="fa fa-sign-in-alt fa-lg"></span>Feedback</button>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                );
+            })}
+            </div>
+            </>
+        );
+    }
+
+}
+
+export default CustomerAppliance;
+
---
+++ src/Components/CustomerBarber.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import {Card} from 'reactstrap';
+import {Link} from 'react-router-dom';
+class CustomerBarber extends Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+        }
+    }
+
+    render(){
+        return(
+            <>
+            <div className="row">
+                <div className="col-12 container-fluid">
+                    <h2 className="feature-heading container-fluid ">Barbers</h2>
+                    <hr className="feature-line" />
+                </div>
+            </div>
+            <div className="container-fluid row mb-5">
+            {this.props.Barbers.map((barber)=>{
+                return(
+                        <div className="col-md-6 mt-3">
+                            <div className="card">
+                                <div className="card-header color" style={{fontWeight:'bold'},{fontSize:"20px"}}>
+                                    {barber.ServiceProvider_Username}
+                                </div>
+                                <div className="card-body">
+                                    <div className="row">
+                                        <h5 className="card-title col-md-5">Name: {barber.ServiceProvider_Username}
+                                        </h5>
+                                        <h5 className="card-title col-md-7">MobileNo: {barber.MobileNo}
+                                        </h5>
+                                    </div>
+                                    <div className="row">
+                                        <h5 className="card-title col-md-5">Average Price: {barber.Price}
+                                        </h5>
+                                        <h5 className="card-title col-md-7">Gender: {barber.Gender}
+                                        </h5>
+                                    </div>
+                                    <div className="row">
+                                        <h5 className="card-title col-md-5">City: {barber.City}
+                                        </h5>
+                                        <h5 className="card-title col-md-7">Occupation: {barber.Occupation}
+                                        </h5>
+                                    </div>
+                                    <div className="row">
+                                        <h5 className="card-title col-md-12">EmailID: {barber.email}
+                                        </h5>
+                                    </div>
+                                    <div className="row">
+                                        <h5 className="card-title col-md-12">Description: {barber.Description}
+                                        </h5>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <Link to={`/Customer/Booking/${barber.ServiceProvider_Username}`}>
+                                                <button className="btn btn-primary btn-md btn-block mt-2 color"  ><span className="fa fa-sign-in-alt fa-lg"></span>Book Service</button>
+                                            </Link>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <Link to={`/Customer/Feedback/${barber.ServiceProvider_Username}`}>
+                                                <button className="btn btn-primary btn-md btn-block mt-2 color"  ><span className="fa fa-sign-in-alt fa-lg"></span>Feedback</button>
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                );
+            })}
+            </div>
+            </>
+        );
+    }
+
+}
+
+export default CustomerBarber;
+
