9cec5643dbc17bb5579c68a17196828df675be6d
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2021-05-06 20:15:43 +0530

    Update CustomerPlumbers.js

--- src/Components/CustomerPlumbers.js
+++ src/Components/CustomerPlumbers.js
@@ -55,14 +55,9 @@ class CustomerPlumbers extends Component{
                                         </h5>
                                     </div>
                                     <div className="row">
-                                        <div className="col-md-6">
+                                        <div className="col-sm-12">
                                             <Link to={`/Customer/Booking/${plumber.ServiceProvider_Username}`}>
                                                 <button className="btn btn-primary btn-md btn-block mt-2 color"  >Book Service</button>
-                                            </Link>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <Link to={`/Customer/Feedback/${plumber.ServiceProvider_Username}`}>
-                                                <button className="btn btn-primary btn-md btn-block mt-2 color"  >Feedback</button>
                                             </Link>
                                         </div>
                                     </div>
